6fd01ecd4f3121a7f8c559d9dfc3187c6054df6b
Eugene-yandex <154636169+Eugene-yandex@users.noreply.github.com>
2024-05-06 22:24:07 +0300

    Update stat_reader.h

--- transport-catalogue/stat_reader.h
+++ transport-catalogue/stat_reader.h
@@ -1,1 +1,10 @@
-// место для вашего кода
+#pragma once
+
+#include <iosfwd>
+#include <string_view>
+
+#include "transport_catalogue.h"
+std::pair<std::string_view, std::string_view> ParseStatReader(std::string_view line);
+
+void PrintStat(const catalog::TransportCatalogue& tansport_catalogue, std::string_view request,
+    std::ostream& output);
