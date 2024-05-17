942cbe7fa45b5caa54b5521b99de99b18bd3451d
Eugene-yandex <154636169+Eugene-yandex@users.noreply.github.com>
2024-05-17 15:45:56 +0300

    Update stat_reader.h

--- transport-catalogue/stat_reader.h
+++ transport-catalogue/stat_reader.h
@@ -4,7 +4,21 @@
 #include <string_view>
 
 #include "transport_catalogue.h"
-std::pair<std::string_view, std::string_view> ParseStatReader(std::string_view line);
+namespace out {
+    struct CommandDescription {
+        std::string_view command;      // Название команды
+        std::string_view id;           // id маршрута или остановки
+    };
+    CommandDescription ParseStatCommand(std::string_view line);
 
-void PrintStat(const catalog::TransportCatalogue& tansport_catalogue, std::string_view request,
-    std::ostream& output);
+    void PrintStat(const catalog::TransportCatalogue& tansport_catalogue, std::string_view request,
+        std::ostream& output);
+
+    void PrintBus(const catalog::TransportCatalogue& tansport_catalogue, std::string_view id,
+        std::ostream& output);
+
+    void PrintStop(const catalog::TransportCatalogue& tansport_catalogue, std::string_view id,
+        std::ostream& output);
+
+    void GetInformation(std::istream& in, const catalog::TransportCatalogue& tansport_catalogue);
+}
