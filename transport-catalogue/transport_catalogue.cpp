07f6a799fb156f3b3f92fb6b47783902ce8020d5
Eugene-yandex <154636169+Eugene-yandex@users.noreply.github.com>
2024-05-17 15:46:17 +0300

    Update transport_catalogue.cpp

--- transport-catalogue/transport_catalogue.cpp
+++ transport-catalogue/transport_catalogue.cpp
@@ -5,79 +5,74 @@
 
 namespace catalog {
 
-	void TransportCatalogue::AddStop(std::string stop, Coordinates coordinates) {
-		stops_.push_back({ std::move(stop),std::move(coordinates) });
+	void TransportCatalogue::AddStop(const std::string& stop, const Coordinates& coordinates) {
+		stops_.push_back({ stop,coordinates });
 		search_stop_.insert({ stops_.back().name, &stops_.back() });
 		buses_of_stop.insert({ &stops_.back(), {} });
 	}
 
-	void TransportCatalogue::AddBus(std::string bus, std::vector<std::string_view> stops) {
+	void TransportCatalogue::AddBus(const std::string& bus, const std::vector<std::string_view>& stops) {
 		std::vector<Stop*> result;
-		buses_.push_back({ std::move(bus), result });
+		buses_.push_back({ bus, result });
 		for (auto stop : stops) {
-			result.push_back(search_stop_.at(stop));
-			buses_of_stop.at(search_stop_.at(stop)).push_back(&buses_.back());
+			auto stop_ptr = search_stop_.at(stop);
+			result.push_back(stop_ptr);
+			if (std::find(buses_of_stop.at(stop_ptr).begin(), buses_of_stop.at(stop_ptr).end(), bus) == buses_of_stop.at(stop_ptr).end()) {
+				buses_of_stop.at(stop_ptr).push_back(buses_.back().name);
+			}
 		}
 		buses_.back().marshrut = std::move(result);
 		search_bus_.insert({ buses_.back().name, &buses_.back() });
 
 	}
 
-	TransportCatalogue::Stop& TransportCatalogue::FindStop(std::string_view stop) const {
-		assert(search_stop_.count(stop) > 0);
-		return *search_stop_.at(stop);
+	TransportCatalogue::Stop* TransportCatalogue::FindStop(std::string_view stop) const {
+		return search_stop_.count(stop) > 0 ? search_stop_.at(stop) : nullptr;
 	}
 
-	TransportCatalogue::Bus& TransportCatalogue::FindBus(std::string_view bus) const {
-		assert(search_bus_.count(bus) > 0);
-		return *search_bus_.at(bus);
+	TransportCatalogue::Bus* TransportCatalogue::FindBus(std::string_view bus) const {
+		return search_bus_.count(bus) > 0 ? search_bus_.at(bus) : nullptr;
 	}
 
-	std::vector<double> TransportCatalogue::BusInfo(std::string_view bus) const {
-		std::vector<double> result;
+	TransportCatalogue::BusInformation TransportCatalogue::GetBusInfo(std::string_view bus) const {
 
-		if (search_bus_.count(bus) == 0) {
+		BusInformation result;
+		auto bus_ptr = FindBus(bus);
+		if (!bus_ptr) {
 			return result;
 		}
-		result.push_back(FindBus(bus).marshrut.size() * 1.0);
+		result.count_stops = bus_ptr->marshrut.size();
 
-		int unikal_stop = 0;
-		double route_length = 0.0;
-		for (size_t x = 0; x < FindBus(bus).marshrut.size(); ++x) {
-			auto stop = FindBus(bus).marshrut[x];
-			auto poisk = std::find(FindBus(bus).marshrut.begin(), FindBus(bus).marshrut.begin() + x, stop);
-			if (poisk == FindBus(bus).marshrut.begin() + x) {
-				++unikal_stop;
+		for (size_t stop_index = 0; stop_index < result.count_stops; ++stop_index) {
+			auto stop = bus_ptr->marshrut[stop_index];
+			auto poisk = std::find(bus_ptr->marshrut.begin(), bus_ptr->marshrut.begin() + stop_index, stop);
+			if (poisk == bus_ptr->marshrut.begin() + stop_index) {
+				++result.unique_stops;
 			}
-			if (x == FindBus(bus).marshrut.size() - 1) {
+			if (stop_index == result.count_stops - 1) {
 				continue;
 			}
-			auto next_stop = FindBus(bus).marshrut[x + 1];
-			route_length += ComputeDistance(stop->stops_coordinat, next_stop->stops_coordinat);
+			auto next_stop = bus_ptr->marshrut[stop_index + 1];
+			result.route_length += ComputeDistance(stop->stops_coordinat, next_stop->stops_coordinat);
 		}
-		result.push_back(unikal_stop);
-		result.push_back(route_length);
 
 		return result;
 	}
-	std::vector<std::string> TransportCatalogue::StopInfo(std::string_view stop) const {
-		std::vector<std::string> result;
+
+	std::vector<std::string_view> TransportCatalogue::GetStopInfo(std::string_view stop) const {
+		std::vector<std::string_view> result;
 		using namespace std::literals;
 		if (search_stop_.count(stop) == 0) {
 			return result;
 		}
 		else if (buses_of_stop.at(search_stop_.at(stop)).empty()) {
-			result.push_back("no buses"s);
+			result.push_back("no buses"sv);
+			return result;
 		}
 		else {
-			for (auto x : buses_of_stop.at(search_stop_.at(stop))) {
-				if (std::find(result.begin(), result.end(), x->name) == result.end()) {
-					result.push_back(x->name);
-				}
-			}
-
+			result = buses_of_stop.at(search_stop_.at(stop));
 			std::sort(result.begin(), result.end());
+			return result;
 		}
-		return result;
 	}
 }
