6d606f719caa2302ebc770b6bc3af423394e2927
Eugene-yandex <154636169+Eugene-yandex@users.noreply.github.com>
2024-06-02 21:53:14 +0300

    Update transport_catalogue.h

--- transport-catalogue/transport_catalogue.h
+++ transport-catalogue/transport_catalogue.h
@@ -32,7 +32,12 @@ namespace catalog {
 
 		int count_stops = 0;
 		int unique_stops = 0;
-		double route_length = 0.0;
+		int route_length_real = 0;
+		double route_length_geographical_coordinates = 0.0;
+	};
+
+	struct PairStopsHasher {
+		size_t operator() (const std::pair<Stop*, Stop*>& pair_stops) const;
 	};
 
 	class TransportCatalogue {
@@ -40,13 +45,14 @@ namespace catalog {
 		TransportCatalogue() = default;
 
 		void AddStop(const std::string& stop, const Coordinates& coordinates);
+		void AddDistance(const std::string_view stop, const std::unordered_map<std::string, int>& all_distance);
 		void AddBus(const std::string& bus, const std::vector<std::string_view>& stops);
 
 		const Stop* FindStop(std::string_view stop) const;
 		const Bus* FindBus(std::string_view bus) const;
 
 		BusInformation GetBusInfo(std::string_view bus) const;
-		std::vector<std::string_view> GetStopInfo(std::string_view stop) const;
+		const std::vector<std::string_view> GetStopInfo(std::string_view stop) const;
 
 
 	private:
@@ -55,6 +61,8 @@ namespace catalog {
 		std::unordered_map<std::string_view, Stop*> search_stop_;
 		std::unordered_map<std::string_view, Bus*> search_bus_;
 		std::unordered_map<Stop*, std::vector<std::string_view>> buses_of_stop;
+		std::unordered_map<std::pair<Stop*, Stop*>, int, PairStopsHasher> distance_stops;
 
+		int ComputeDistanceRealDistance(Stop* from, Stop* to) const;
 	};
 }
