df01f5be40cc3a00473d941c657adb6d44c99887
Eugene-yandex <154636169+Eugene-yandex@users.noreply.github.com>
2024-06-27 16:56:41 +0300

    Update main.cpp

--- transport-catalogue/main.cpp
+++ transport-catalogue/main.cpp
@@ -8,7 +8,7 @@ using namespace std;
 
 int main() {
     catalog::TransportCatalogue catalogue;
-    jreader::JsonInformation ji(cin);
+    jreader::JSONReader ji(cin);
     ji.CreateDatabase(catalogue);
     ji.PrintStat(catalogue, cout);
 }
